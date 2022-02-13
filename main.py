96b290b5867a6dd49a3cd473b611b8fd1c3960cb
Dahkilgov <m.dahkilgov@redcode.vip>
2022-02-13 11:05:40 +0300

    Added activation by name

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import os
 def talk():
     input = sr.Recognizer()
     with sr.Microphone() as source:
+        input.adjust_for_ambient_noise(source, duration=0.2)
         audio = input.listen(source)
         data = ""
         try:
@@ -37,48 +38,65 @@ def respond(output):
 if __name__ == '__main__':
     respond("Приветствую сэр")
 
+    webbrowser.register('edge', None, webbrowser.BackgroundBrowser("C://Program Files (x86)//Microsoft//Edge//Application//msedge.exe"))
+    webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C://Program Files (x86)//Google//Chrome//Application//chrome.exe"))
+
     while (1):
-        respond("Чем могу быть полезна?")
+        # respond("Чем могу быть полезна?")
         text = talk().lower()
 
         if text == 0:
             continue
 
-        if "стоп" in str(text) or "пока" in str(text) or "уйди" in str(text):
-            respond("Всего наилучшего сэр!")
-            break
+        skyle = ["скайла", "ская", "сказал", "скаи", "кайл", "сказку", "сказка", "скажи", 'skill', "скала", "скал", "сушистайл", "слушать", "сканер", "сушиста", 'skype', 'skyla']
 
-        if 'найди в википедии' in text:
-            respond('ищу в википедии')
-            text = text.replace("найди в википедии", "")
-            wikipedia.set_lang('ru')
-            results = wikipedia.summary(text, sentences=3)
-            respond("вот что я нашла")
-            print(results)
-            respond(results)
+        for x in skyle:
+            if x in text:
+                text = text.replace(x, 'skyle')
 
-        elif 'сколько время' in text:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            respond(f"сейчас время {strTime}")
+                if "skyle" in text:
 
-        elif 'найди' in text:
-            text = text.replace("найди", "")
-            webbrowser.open_new_tab(text)
-            time.sleep(5)
+                    if "стоп" in str(text) or "пока" in str(text) or "уйди" in str(text):
+                        respond("Всего наилучшего сэр!")
+                        break
 
-        elif 'открой google' in text:
-            webbrowser.open_new_tab("https://www.google.com")
-            respond("открываю google")
-            time.sleep(5)
+                    if "найди в википедии" in text:
+                        respond('ищу в википедии')
+                        text = text.split("найди в википедии ")[-1]
+                        print(text)
+                        # text = text.replace("найди в википедии", "")
+                        wikipedia.set_lang('ru')
+                        results = wikipedia.summary(text, sentences=3)
+                        respond("вот что я нашла")
+                        print(results)
+                        respond(results)
 
-        elif 'открой youtube' in text:
-            webbrowser.open_new_tab("https://www.youtube.com")
-            respond("открываю youtube")
-            time.sleep(5)
+                    elif 'сколько время' in text:
+                        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+                        respond(f"сейчас время {strTime}")
 
-        elif "открой word" in text:
-            respond("открываю Microsoft Word")
-            os.startfile("C:\Program Files (x86)\Microsoft Office\\root\Office16\WINWORD.EXE")
+                    elif 'найди' in text:
+                        text = text.split("найди ")[-1]
+                        # text = text.replace("найди", "")
+                        webbrowser.get('edge').open_new_tab(text)
+                        # time.sleep(5)
 
-        else:
-            respond("Приложение не доступно")
+                    elif 'google' in text or 'гугл' in text:
+                        webbrowser.get('edge').open_new_tab("https://www.google.com")
+                        respond("открываю google")
+                        # time.sleep(5)
+
+                    elif 'youtube' in text:
+                        webbrowser.get('edge').open_new_tab("https://www.youtube.com")
+                        respond("открываю youtube")
+                        # time.sleep(5)
+
+                    elif "word" in text:
+                        respond("открываю Microsoft Word")
+                        os.startfile("C://Program Files (x86)//Microsoft Office//root//Office16//WINWORD.EXE")
+
+                    else:
+                        # respond("Приложение не доступно")
+                        respond("Чем могу быть полезна?")
+                        break
+                        # break
